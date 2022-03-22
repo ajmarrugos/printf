7fd09ffe3bd94ea4202ab070394e8d4d10f8ddde
Kadzahk <juan427lol@gmail.com>
2022-03-22 12:27:53 -0500

    func to print a char

---
+++ print_char.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* print_char - Print formated
+* @lst: va_arg
+* Return: String to print
+*/
+
+char *print_char(va_list lst)
+{
+	char *the_list;
+	char charV;
+
+	if (!lst)
+		return (0);
+
+	charV = va_arg(lst, int);
+	the_list = malloc(sizeof(char) * 2);
+	if (!the_list)
+	{
+		free(the_list);
+		return (NULL);
+	}
+	if (charV)
+	{
+		the_list[0] = charV;
+	}
+	else
+	{
+		the_list[0] = '\0';
+	}
+	the_list[1] = '\0';
+return (the_list);
+}
